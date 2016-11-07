1863aa23a098d9d596b60a4c268c7a6f1c22499d
Michael9378 <michael@lucidagency.com>
2016-11-07 13:17:07 -0700

    Adding script to pull utm's from url and create hidden fields from them

---
+++ utm_hack.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+	// grabs the utm's from a url and add them to hidden fields in a .utm_sources div
+	// see if there are any params
+	if( window.location.href.indexOf("?")!= -1 ) {
+		// split the params
+		var paramsUnsplit = window.location.href.split("?")[1].split("&");
+		// loop through the params and grab the key and values
+		for(var i = 0; i < paramsUnsplit.length; i++) {
+			var key = paramsUnsplit[i].split("=")[0];
+			var value = paramsUnsplit[i].split("=")[1];
+			// if it is a utm param, append it to the utm div.
+			if( key.indexOf('utm')!= -1 ) {
+				// this is a utm, append it to utm box
+				jQuery(".utm_sources").append('<input type="hidden" name="' + key + '" value="' + value + '">');
+			}
+		}
+	}
+});
